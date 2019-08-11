921f8d4a044271b3d45fafb7ef96755c978d8bc0
matacz <matacz@gmail.com>
2019-08-11 13:05:11 +0200

    add test on multiplication and substraction

--- src/test/java/pl/sda/spring/CalculatorTest.java
+++ src/test/java/pl/sda/spring/CalculatorTest.java
@@ -18,13 +18,38 @@ public class CalculatorTest {
     @Test
     public void shouldReturnFiveForGivenArgumentsOnAdditionOperation() {
         //given
-        double arg1 = 2.0;
-        double arg2 = 3.0;
+        double arg1 = 3.0;
+        double arg2 = 2.0;
 
         //when
         double actual = calculator.calculate(OperationType.ADDITION, arg1, arg2);
 
         //then
         Assert.assertEquals(5.0, actual, 0.00000001);
+    }
+
+    @Test
+    public void shouldReturnFiveForGivenArgumentsOnSubtractionOperation() {
+        //given
+        double arg1 = 3.0;
+        double arg2 = 2.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.SUBTRACTION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(1.0, actual, 0.00000001);
+    }
+    @Test
+    public void shouldReturnFiveForGivenArgumentsOnMultiplicationOperation() {
+        //given
+        double arg1 = 3.0;
+        double arg2 = 2.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.MULTIPLICATION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(6.0, actual, 0.00000001);
     }
 }
